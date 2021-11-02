3ac46424bd3cab5aa3dd060efceb85e42db1ec1a
Carlos González <cgonzalez@leadgogo.com>
2021-11-02 00:41:34 +0000

    username select dropdown

--- apps/frontend/src/components/sign-in.tsx
+++ apps/frontend/src/components/sign-in.tsx
@@ -1,17 +1,40 @@
 import { FormEvent, memo, useCallback, useState } from 'react';
 
-export const SignIn = memo<{ setUsername: (username: string) => void }>(({ setUsername }) => {
-  const [value, setValue] = useState<string>('');
+const usernames = ['carlos', 'lina'];
 
-  const handleSubmit = useCallback((event: FormEvent) => {
-    event.preventDefault();
-    setUsername(value)
-  }, [value, setUsername]);
+export const SignIn = memo<{ setUsername: (username: string) => void }>(
+  ({ setUsername }) => {
+    const [value, setValue] = useState<string>('');
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={value} onChange={(event) => setValue(event.target.value)} />
-      <button type="submit">Set username</button>
-    </form>
-  )
-})
+    const handleSubmit = useCallback(
+      (event: FormEvent) => {
+        event.preventDefault();
+        if (!usernames.some((u) => u === value)) {
+          alert('chose a valid username');
+        } else {
+          setUsername(value);
+        }
+      },
+      [value, setUsername]
+    );
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <select
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        >
+          <option value='' disabled>
+            choose
+          </option>
+          {usernames.map((username) => (
+            <option key={username} value={username}>
+              {username}
+            </option>
+          ))}
+        </select>
+        <button type='submit'>Set username</button>
+      </form>
+    );
+  }
+);

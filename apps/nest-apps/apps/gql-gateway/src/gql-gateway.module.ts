37ddab1dfcb8a80e5c9b68c92071a212f389b7e1
Carlos González <cgonzalez@leadgogo.com>
2021-11-02 00:22:38 +0000

    gateway delay

--- apps/nest-apps/apps/gql-gateway/src/gql-gateway.module.ts
+++ apps/nest-apps/apps/gql-gateway/src/gql-gateway.module.ts
@@ -3,26 +3,31 @@ import { MercuriusGatewayModule } from 'nestjs-mercurius';
 
 @Module({
   imports: [
-    MercuriusGatewayModule.forRoot({
-      graphiql: true,
-      subscription: true,
-      allowBatchedQueries: true,
-      gateway: {
-        pollingInterval: 1_000,
-        services: [
-          {
-            name: 'subscriptions',
-            url: 'http://localhost:3002/graphql',
-            wsUrl: 'ws://localhost:3002/graphql',
-            mandatory: true,
+    MercuriusGatewayModule.forRootAsync({
+      useFactory: async () => {
+        await new Promise((resolve) => setTimeout(resolve, 5_000));
+        return {
+          graphiql: true,
+          subscription: true,
+          allowBatchedQueries: true,
+          gateway: {
+            pollingInterval: 1_000,
+            services: [
+              {
+                name: 'subscriptions',
+                url: 'http://localhost:3002/graphql',
+                wsUrl: 'ws://localhost:3002/graphql',
+                mandatory: true,
+              },
+              {
+                name: 'messages',
+                url: 'http://localhost:3001/graphql',
+                // wsUrl: 'ws://localhost:3001/graphql',
+                mandatory: true,
+              },
+            ],
           },
-          {
-            name: 'messages',
-            url: 'http://localhost:3001/graphql',
-            // wsUrl: 'ws://localhost:3001/graphql',
-            mandatory: true,
-          },
-        ],
+        };
       },
     }),
   ],
